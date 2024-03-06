b4283d7b61ba5bf5264b2399d832d4e512cb392e
mikemwai <mwai.michael@strathmore.edu>
2024-03-06 13:20:40 +0300

    added put method

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -57,5 +57,24 @@ namespace SuperHeroAPI.Controllers
 
             return Ok(await _context.SuperHeroes.ToListAsync());
         }
+        
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero updatedHero)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(updatedHero.Id);
+            if (dbHero == null)
+            {
+                return BadRequest("Hero not found.");
+            }
+
+            dbHero.Name = updatedHero.Name;
+            dbHero.FirstName = updatedHero.FirstName;
+            dbHero.LastName = updatedHero.LastName;
+            dbHero.Place = updatedHero.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
     }
 }
